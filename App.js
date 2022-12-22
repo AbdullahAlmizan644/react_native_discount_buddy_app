f72e3f4be55b5473b9877fdd647bdd12d5981c9c
AbdullahAlmizan644 <abdullahalmizan644@gmail.com>
2022-12-22 15:50:43 +0600

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {WebView} from 'react-native-webview';
+ 
+const buddy="http://discountbuddy.pythonanywhere.com/";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <View style={{ width:'100%', height:'100%'}}>
+       <WebView
+        source={{ uri: buddy }}
+      />
+
+       </View>
+
       <StatusBar style="auto" />
+      
     </View>
   );
 }
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
